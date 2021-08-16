150c7084130fe7eb90b2219a55a32ef6adcd1129
Luciano1991 <lucianossdp@gmail.com>
2021-08-16 11:46:25 -0300

    Entrega 1
    Petición web a una URL donde se encuentra una colección de productos en formato JSON, con la información básica (precio, nombre, breve descripción) respectiva a cada producto, y mostrar el listado en HTML.
    Se agrega una pantalla de autenticación, de forma tal que la misma sea el inicio del sitio (en vez de la portada donde se muestra el listado de categorías).
    Se agregan las validaciones correspondientes de usuario y contraseña (campos no vacíos) y redireccionar al sitio de portada cuando presione el botón de Ingresar

--- js/login.js
+++ js/login.js
@@ -1,6 +1,65 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+
+//Definiremos un evento "submit" para capturar el instante en el que el usuario enviará el formulario.
+//Una vez capturado el evento submit del formulario con Javascript, vamos bloquear el envío, evitando que pueda enviar con datos incorrectos.
+//Validaremos todos los campos del formulario.
+//Si todos los campos se validan correctamente, entonces realizaremos el envío nosotros mediante Javascript.
+document.addEventListener("DOMContentLoaded", function(){
+    
 
 });
+
+
+function login(name, password){
+    
+    if (name == ""){
+      return true;
+    }
+
+    if (password == ""){
+      return true;
+    }
+
+    
+    
+}
+
+
+function checkLogin(){
+    
+    let nombre = document.getElementById('user').value
+    let password = document.getElementById('pass').value
+
+    if (login(nombre, password) == true){
+    alert('Campos incompletos, por favor completar');
+    } else {
+          window.location.replace("home.html");
+        console.log('inicio exitoso');
+    }
+}
+
+
+
+
+
+//document.getElementById("login").addEventListener('submit', validarAcceso); 
+
+/*function validarAcceso(evento) {
+    evento.preventDefault();
+    let user = document.getElementById('user').value;
+    if(user.length == 0) {
+      alert('No has escrito nada en el usuario');
+      return;
+    }
+    let pass = document.getElementById('password').value;
+    if (pass.length ==0) {
+      alert('La clave no es válida');
+      return;
+    }
+    this.submit();
+  }*/
+
+/*let usuario = document.getElementById("user").value ;
+let usuario = document.getElementById("password").value ;*/
--- js/products.js
+++ js/products.js
@@ -1,6 +1,35 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+fetch(PRODUCTS_URL)
+.then(Response => Response.json() )
+.then(data => {
+let contenedor=document.getElementById('contenedor')
+let contenido="";
+for (let p = 0; p < data.length; p++) {
+    contenido+=
+    `
+        <h1>${data[p].name}</h1>
+        <p><img src=${data[p].imgSrc} +  alt=""></p>
+        <h4 style="width:780px">${data[p].description}</h4>
+        <br>
+        <h5>${data[p].cost} ${data[p].currency} </h6>
+        
+        <h5>${'Unidades vendidas: ' + data[p].soldCount}</h4>
+        <br><br>
+    `
+}    
+contenedor.innerHTML= contenido;
+     
 
+})
+.catch(err=>console.log(err));
+
+
+//1. Obtener la información. Para eso usamos la función FETCH o la función GETJSONDATA definida en init.js. 
+//2. Hacer procedimiento para insertar en HTML el primer producto. 
+//3. Hacer prodecimiento para insertar todos los productos en el HTML.
+
+
+
+document.addEventListener("DOMContentLoaded", function (e){
+   
 });
+
