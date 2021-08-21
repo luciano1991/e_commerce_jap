b31acac457bc3892e5bf51b583d8c6a2329a8686
Luciano1991 <lucianossdp@gmail.com>
2021-08-21 15:26:43 -0300

    Entrega 1 último
    Trabajo de entrega uno. Queda sin terminar (en comentarios)la validación mediante Google.

--- js/login.js
+++ js/login.js
@@ -1,65 +1,26 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-
-//Definiremos un evento "submit" para capturar el instante en el que el usuario enviará el formulario.
-//Una vez capturado el evento submit del formulario con Javascript, vamos bloquear el envío, evitando que pueda enviar con datos incorrectos.
-//Validaremos todos los campos del formulario.
-//Si todos los campos se validan correctamente, entonces realizaremos el envío nosotros mediante Javascript.
 document.addEventListener("DOMContentLoaded", function(){
     
 
 });
 
-
+//Funcion que checkea que los campos no sean vacíos
 function login(name, password){
     
-    if (name == ""){
-      return true;
-    }
-
-    if (password == ""){
-      return true;
-    }
-
-    
+  return (name!=="") && (password!=="")
     
 }
 
-
+//Función que redirige a la pagina principal
 function checkLogin(){
     
     let nombre = document.getElementById('user').value
     let password = document.getElementById('pass').value
 
-    if (login(nombre, password) == true){
-    alert('Campos incompletos, por favor completar');
+    if (login(nombre, password)){ 
+      window.location.replace("home.html");
+      console.log('inicio exitoso');
     } else {
-          window.location.replace("home.html");
-        console.log('inicio exitoso');
+    
+       document.getElementById('alerta').removeAttribute('hidden');
     }
 }
-
-
-
-
-
-//document.getElementById("login").addEventListener('submit', validarAcceso); 
-
-/*function validarAcceso(evento) {
-    evento.preventDefault();
-    let user = document.getElementById('user').value;
-    if(user.length == 0) {
-      alert('No has escrito nada en el usuario');
-      return;
-    }
-    let pass = document.getElementById('password').value;
-    if (pass.length ==0) {
-      alert('La clave no es válida');
-      return;
-    }
-    this.submit();
-  }*/
-
-/*let usuario = document.getElementById("user").value ;
-let usuario = document.getElementById("password").value ;*/
--- js/products.js
+++ js/products.js
@@ -1,3 +1,8 @@
+//1. Obtener la información. Para eso usamos la función FETCH o la función GETJSONDATA definida en init.js. 
+//2. Hacer procedimiento para insertar en HTML el primer producto. 
+//3. Hacer prodecimiento para insertar todos los productos en el HTML.
+
+
 fetch(PRODUCTS_URL)
 .then(Response => Response.json() )
 .then(data => {
@@ -6,13 +11,13 @@ let contenido="";
 for (let p = 0; p < data.length; p++) {
     contenido+=
     `
-        <h1>${data[p].name}</h1>
-        <p><img src=${data[p].imgSrc} +  alt=""></p>
-        <h4 style="width:780px">${data[p].description}</h4>
+        <h1  class="d-flex w-100 justify-content-between">${data[p].name}</h1>
+        <p><img src=${data[p].imgSrc} +  alt="" + " class="img-thumbnail"></p>
+        <h4 class="description">${data[p].description}</h4>
         <br>
-        <h5>${data[p].cost} ${data[p].currency} </h6>
+        <h5 class="mb-1 text-muted ">${'Valor: ' + " " + data[p].cost} ${data[p].currency} </h5>
         
-        <h5>${'Unidades vendidas: ' + data[p].soldCount}</h4>
+        <h5 class="mb-1 text-muted ">${'Unidades vendidas: ' + data[p].soldCount}</h5>
         <br><br>
     `
 }    
@@ -23,9 +28,6 @@ contenedor.innerHTML= contenido;
 .catch(err=>console.log(err));
 
 
-//1. Obtener la información. Para eso usamos la función FETCH o la función GETJSONDATA definida en init.js. 
-//2. Hacer procedimiento para insertar en HTML el primer producto. 
-//3. Hacer prodecimiento para insertar todos los productos en el HTML.
 
 
 
